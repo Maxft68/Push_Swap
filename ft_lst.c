16540903790dd1d1bcc35f944f7ca0aee1a21a51
Max <mdsiurds@gmail.com>
2025-02-04 20:12:52 +0100

    pushdansbokvalgrindpasfais

--- ft_lst.c
+++ ft_lst.c
@@ -6,7 +6,7 @@
 /*   By: mdsiurds <mdsiurds@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/31 11:41:30 by mdsiurds          #+#    #+#             */
-/*   Updated: 2025/02/04 17:21:56 by mdsiurds         ###   ########.fr       */
+/*   Updated: 2025/02/04 20:10:30 by mdsiurds         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,11 @@
 
 typedef struct s_list
 {
-	int			*content;
+	int				value;
 	struct s_list	*next;
 	struct s_list	*prev;
 }					t_list;
+
 
 // void ft_push(t_list *lst_sent, t_list *lst_to_received)
 // {
@@ -57,7 +58,7 @@ int ft_lstsize(t_list *list)
 	t_list *temp;
 	temp = list;
 	int i;
-	i = 0;
+	i = 1;
 	if (list == NULL)
 		return (0);
 	while (temp->next != list)
@@ -65,19 +66,17 @@ int ft_lstsize(t_list *list)
 		i++;
 		temp = temp->next;
 	}
-	if (list->next != temp)
-		i++;
 	return (i);
 }
 
-t_list *ft_lstnew(void *value) //(void *value, void *index) ??
+t_list *ft_lstnew(char *the_value)
 {
 	t_list *new;
 	
 	new = malloc(sizeof(t_list));
 	if(!new)
 		return(NULL);
-	new->content = value;
+	new->value = atoi(the_value);
 	new->next = NULL; // = (*new).next
 	new->prev = NULL;
 	return(new);
@@ -112,6 +111,25 @@ t_list *ft_lstadd_back(t_list *lst, t_list *new)
 	
 // }
 
+t_list *first_push(t_list **lst_sent) // stack B empty
+{	
+	
+	t_list *new_stack;
+	
+	new_stack = NULL;
+	
+	new_stack = *lst_sent;
+	
+	*lst_sent = (*lst_sent)->next;
+	(*lst_sent)->prev->prev->next = *lst_sent; //extraction
+	(*lst_sent)->prev = (*lst_sent)->prev->prev; //extraction
+	new_stack->next = new_stack; //boucle sur sois meme
+	new_stack->prev = new_stack; //boucle sur sois meme
+	printf("contenueNEW = %d;", new_stack->value);
+	printf(" prevNEW = %d;", new_stack->prev->value);
+	printf(" nextNEW = %d\n", new_stack->next->value);
+	return (new_stack);
+}
 int main()
 {
 	t_list *head;
@@ -121,14 +139,22 @@ int main()
 	t_list *j;
 	t_list *k;
 	t_list *l;
-	head = ft_lstnew("hello");
-	i = ft_lstnew("1node");
+	char *un = "1";
+	char *deux = "2";
+	char *trois = "3";
+	char *quatre = "4";
+	char *cinq = "5";
+	//char *six = "6";
+	
+	
+	head = ft_lstnew(un);
+	i = ft_lstnew(deux);
 	head = ft_lstadd_back(head, i);
-	j = ft_lstnew("2node");
+	j = ft_lstnew(trois);
 	head = ft_lstadd_back(head, j);
-	k = ft_lstnew("3node");
+	k = ft_lstnew(quatre);
 	head = ft_lstadd_back(head, k);
-	l = ft_lstnew("4node");
+	l = ft_lstnew(cinq);
 	head = ft_lstadd_back(head, l);
 
 	// t_list *head2;
@@ -149,25 +175,47 @@ int main()
 	tmp = head;
 	while(tmp->next != head)
 	{
-		printf("contenue = %s;", (char *)tmp->content);
-		printf(" prev = %s;", (char *)tmp->prev->content);
-		printf(" next = %s\n", (char *)tmp->next->content);
+		printf("contenue = %d;", tmp->value);
+		printf(" prev = %d;",tmp->prev->value);
+		printf(" next = %d\n",tmp->next->value);
 		tmp = tmp->next;
 		if(tmp->next == head)
 		{
-			printf("contenue = %s;", (char *)tmp->content);
-			printf(" prev = %s;", (char *)tmp->prev->content);
-			printf(" next = %s\n", (char *)tmp->next->content);
+			printf("contenue = %d;", tmp->value);
+			printf(" prev = %d;", tmp->prev->value);
+			printf(" next = %d\n", tmp->next->value);
 		}
 	}
 	printf("taille de la liste = %d\n", ft_lstsize(head));
-	ft_lstclear(&head);
 	
 	if (head == NULL)
 		printf("LISTE VIDE!! \n");
-	printf("taille de la liste2 = %d\n", ft_lstsize(head));
 	
-	//printf("taille de la liste = %d\n", ft_lstsize(head));
+	t_list *head2;
+
+	head2 = NULL;
+	
+	head2 = first_push(&head);
+	printf("contenueNEW = %d;", head2->value);
+	printf(" prevNEW = %d;", head2->prev->value);
+	printf(" nextNEW = %d\n", head2->next->value);
+	printf("taille de la liste2 = %d\n", ft_lstsize(head2));
+	printf("taille de la liste = %d\n", ft_lstsize(head));
+	tmp = head;
+	while(tmp->next != head)
+	{
+		printf("contenue = %d;", tmp->value);
+		printf(" prev = %d;",tmp->prev->value);
+		printf(" next = %d\n",tmp->next->value);
+		tmp = tmp->next;
+		if(tmp->next == head)
+		{
+			printf("contenue = %d;", tmp->value);
+			printf(" prev = %d;", tmp->prev->value);
+			printf(" next = %d\n", tmp->next->value);
+		}
+	}
+	ft_lstclear(&head);
 	return(0);
 }
 
@@ -182,25 +230,25 @@ int main()
 	
 	// //toto = ft_lstnew(argv[1]);
 	// toto = ft_lstnew("hello");
-	// printf("%s\n", (char *)toto->content);
-	// printf("%p\n", (char *)toto->next); // = NULL car pas encore creer
+	// printf("%s\n", toto->content);
+	// printf("%p\n", toto->next); // = NULL car pas encore creer
 
 	// toto2 = ft_lstnew("hello454");
-	// printf("%s\n", (char *)toto2->content);
-	// printf("%p\n", (char *)toto2->next);// = NULL car pas encore creer
+	// printf("%s\n", toto2->content);
+	// printf("%p\n", toto2->next);// = NULL car pas encore creer
 	// toto->next = toto2; // defini que le next de toto cest toto2
 	// printf("%p\n", toto2); // = toto->next
-	// printf("%p\n", (char *)toto->next); // = toto2
+	// printf("%p\n", toto->next); // = toto2
 
-	// printf("%s\n", (char *)toto->next->content); // content du next de toto / donc content de toto2
+	// printf("%s\n", toto->next->content); // content du next de toto / donc content de toto2
 
 	// tmp = toto; //defini une temp pour pas se deplacer avec toto
 	// while(tmp != NULL) // se deplace dans les nodes tant qu'il y en a 
 	// {
-	// 	printf("%s\n", (char *)tmp->content); // = content de toto
+	// 	printf("%s\n", tmp->content); // = content de toto
 	// 	tmp = tmp->next; // = toto2
 	// }
-	// printf("%s\n", (char *)toto->content); // = content de toto
+	// printf("%s\n", toto->content); // = content de toto
 //}
 
 
@@ -218,7 +266,7 @@ int main()
 	tmp = toto;
 	while(tmp != NULL)
 	{
-		printf("%s\n", (char *)tmp->content);
+		printf("%s\n", tmp->content);
 		tmp = tmp->next;
 	}
 	=
